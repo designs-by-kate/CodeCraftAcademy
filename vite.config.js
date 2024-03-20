4ddcb51aba3b7f749b26adca09542f2623d11448
designs-by-kate <kate_diet@hotmail.com>
2024-03-20 10:19:46 +0000

    Edit github workflows and config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build"
-  },
   css: {
     modules: {
-      localsConvention: "camelCase"
-    }
-  },
-  server: {
-    hmr: {
-      overlay: false,
+      localsConvention: "camelCase",
     },
   },
   base: '/revision-app/',
-});
+})
